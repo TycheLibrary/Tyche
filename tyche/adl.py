2d1c2e6122dc49953a24b3b8a7c85f20776c4819
drtnf <tim.french@uwa.edu.au>
2021-05-10 11:44:52 +0800

    basic formula class

--- tyche/adl.py
+++ tyche/adl.py
@@ -22,62 +22,32 @@ class Concept(tuple):
     The constructor builds atomic concepts, and complex concepts are constructed using the static methods provided.
     '''
 
-    #seems like a waste of space to have strings in the tuples
-    #use constants???
-    #everything can b
-    op2code = {'Yes':0,'No':1,'Atom':2,'Conditional':3,'Marginal':4}
-    code2op = {0:'Yes',1:'No',2:'Atom',3:'Conditional',4:'Marginal'}
-
-    def __new__(cls, operator :str, symbol=None: str, term1=None:Concept, term2=None:Concept, term3=None:Concept, role=None:Role):
+    def __new__(cls, operator, symbol=None, term1=None, term2=None, term3=None, role=None):
         '''Constructs all formulas as tuples'''
         constructors = {#lambdas to construct formulas
-            'Yes': lambda c, s, x, y, z, r: tuple.__new__(c, (0))
-            'No': lambda c, s, x, y, z, r: tuple.__new__(c, (1))
-            'Atom': lambda c, s, x, y, z, r : tuple.__new__(c, (2, x)),
-            'Conditional': lambda c, s, x, y, z, r: tuple.__new__(c, (3,x,y,z)),
-            'Marginal': lambda c, s, x, y, z, r: tuple.__new__(c, (4, r,x,y))
+            'No': lambda c, s, x, y, z, r: tuple.__new__(c, ('No')),
+            'Yes': lambda c, s, x, y, z, r: tuple.__new__(c, ('Yes')),
+            'Atom': lambda c, s, x, y, z, r : tuple.__new__(c, ('Atom', s)),
+            'Conditional': lambda c, s, x, y, z, r: tuple.__new__(c, ('Conditional',x,y,z)),
+            'Marginal': lambda c, s, x, y, z, r: tuple.__new__(c, ('Marginal', r,x,y))
             }#language core
-        if symbol is not None not is_valid_symbol:
-            raise ADLExcpetion('Symbols must begin with lowercase letter, and contain only alphanumeric characters or underscores')
-        if role is not None not is_valid_symbol:
-            raise ADLExcpetion('Roles must begin with lowercase letter, and contain only alphanumeric characters or underscores')
-        if not operator in ['Yes','No','Atom','Conditinal','Marginal']:
-            raise ADLExcpetion('Operator must be Yes, No, Atom, Conditional or Marginal')
-        return constructors.get(operator)(symbol, term1, term2, term3, role)
+        if operator=='Atom' and not cls.is_valid_symbol(symbol):
+            raise ADLException('Symbols must begin with lowercase letter, and contain only alphanumeric characters or underscores')
+        if operator=='Marginal' and not cls.is_valid_symbol(role):
+            raise ADLException('Roles must begin with lowercase letter, and contain only alphanumeric characters or underscores')
+        if not operator in ['Yes','No','Atom','Conditional','Marginal']:
+            raise ADLException('Operator must be Yes, No, Atom, Conditional or Marginal')
+        return constructors.get(operator)(cls, symbol, term1, term2, term3, role)
 
-    def is_valid_symbol(s:str):
+
+    def is_valid_symbol(s):
         '''Checks a string contains only alphanumeric chacaters or underscore'''
-        valid = s not None and len(s)<1 or not s[0].islower()
-        for c in s[1:]:
-            valid = valid and ((c.isalnum) or c=='_')
+        valid = s is not None and len(s)>0  and s[0].islower()
+        if valid:
+            for c in s[1:]:
+                valid = valid and ((c.isalnum) or c=='_')
         return valid    
 
-    #static constructors for different types of formulas
-    yes = Concept('Yes')
-    no = Concept('No')
-    def atom(symbol:str) -> Concept:
-        return Concept('Atom',symbol=symbol)
-    def conditional(condition: Concept, positive: Concept, negative: Concept)-> Concept:
-        return Concept('Conditional', term1=condition, term2=positive, term3=negative)
-    def marginal(role: Role, event: Concept, margin: Concept)-> Concept:
-        return Concept('Marginal', term1=condition, term2=margin, role=role)
-    #some abbreviations provided too
-    def negation(term:Concept) -> Concept:
-        return Concept('Conditional', term1=term, term2=no, term3=yes)
-    def conjunction(left:Concept, right: Concept) -> Concept:
-        return Concept('Conditional', term1=left, term2=right, term3=no)
-    def disjunction(left:Concept, right: Concept) -> Concept:
-        return Concept('Conditional', term1=left, term2=yes, term3=right)
-    def implication(antecedent: Concept, consequent: Concept) -> Concept:
-        return Concept('Conditional', term1=antecedent, term2=consequent, term3=yes)
-    def equivalence(left: Concept, right Concept) -> Concept:
-        return Concept('Conditional', term1=left, term2=right,term3=negation(right))
-    def expectation(role: Role, term: Concept):
-        return Concept('Marginal', term1=term, term2=yes, role=role)
-    def necessity(role: Role, term: Concept):
-        return Concept('Marginal', term1=no, term2=negation(term), role=role)
-    def possibility(role: Role, term: Concept):
-        return negation(Concept('Marginal', term1=no, term2=term, role=role))
 
     #properties included via decorators
     @property
@@ -138,15 +108,15 @@ class Concept(tuple):
             'Atom': lambda x: x[1],
             'Yes': lambda x: 'Y',
             'No': lambda x: 'N',
-            'Conditional': lambda x: '({1}?{2}:{3})'.format(x[1].__repr__(), x[2].__repr__(), x[3].__repr__()),
-            'Marginal': lambda x: '.{1}({2}|{3})'.format(x[1].__repr__(),x[2].__repr__(),x[3].__repr__())
+            'Conditional': lambda x: '({0}?{1}:{2})'.format(x[1].__repr__(), x[2].__repr__(), x[3].__repr__()),
+            'Marginal': lambda x: '.{0}({1}|{2})'.format(x[1].__repr__(),x[2].__repr__(),x[3].__repr__())
             }
-        return reps.get(x[0])(x)
+        return reps.get(self[0])(self)
 
     def pretty(self, indent=0):
         '''Returns a multiline formatted version of the formula.
         Returning repr for now'''
-        return __repr__()
+        return __repr__(self)
 
     #Proof theoretic functions here.
     def normal_form(self) -> 'Concept':
@@ -173,13 +143,13 @@ class Role(tuple):
     Roles are immutable and encoded via tuples. 
     We currently just use atomic roles, but use these structures to allow for dynamic modalities in future.
     '''
-    def __new__(cls, role: str) -> Role:
+    def __new__(cls, role: str):
         '''Creates an atomic concept, with the symbol symbol
            
            symbol should be an alpha-numeric+underscore string, starting with a lower case letter.
            a '.' is prepended to the symbol to distinguish it from a concept.
         '''
-        if(role==None or len(role)<1 or not role[0].is_lower()):
+        if(role==None or len(role)<1 or not role[0].islower()):
             raise ADLException("Naming error, atomic concept symbols must begin with lower case letters")
         return tuple.__new__(cls, ('.'+role))
 
@@ -191,7 +161,7 @@ class Role(tuple):
         '''Gives the concise mathematical representation of the concept
         In this case, just the symbol
         '''
-      return self.role
+        return self.role
 
     def __str__(self):
         '''Gives a formatted version of the concept, using English operators, and pretty indenting
@@ -199,8 +169,31 @@ class Role(tuple):
         '''
         return self.role
 
-
-
-
-
+#static constructors for different types of formulas
+yes = Concept('Yes')
+no = Concept('No')
+#static functions for generating functions
+def atom(symbol):
+    return Concept('Atom',symbol=symbol)
+def conditional(condition, positive, negative):
+    return Concept('Conditional', term1=condition, term2=positive, term3=negative)
+def marginal(role, event, margin):
+    return Concept('Marginal', term1=event, term2=margin, role=role)
+#some abbreviations provided too
+def negation(term):
+    return Concept('Conditional', term1=term, term2=no, term3=yes)
+def conjunction(left, right):
+    return Concept('Conditional', term1=left, term2=right, term3=no)
+def disjunction(left, right):
+    return Concept('Conditional', term1=left, term2=yes, term3=right)
+def implication(antecedent, consequent):
+    return Concept('Conditional', term1=antecedent, term2=consequent, term3=yes)
+def equivalence(left, right):
+    return Concept('Conditional', term1=left, term2=right,term3=negation(right))
+def expectation(role, term):
+    return Concept('Marginal', term1=term, term2=yes, role=role)
+def necessity(role, term):
+    return Concept('Marginal', term1=no, term2=negation(term), role=role)
+def possibility(role, term):
+    return negation(Concept('Marginal', term1=no, term2=term, role=role))
 
