3e9da617baa898cb9b05cd6a53ed68db8f1d764b
drtnf <tim.french@uwa.edu.au>
2021-08-15 21:12:18 +0800

    Updated readme, prototyped adl classes, and added fixedpoints

--- README.md
+++ README.md
@@ -1,19 +1,28 @@
 #tyche
 
-Python implementation of an aleatoric knowledge base, and association utilities.
+Python library to represent and reason about aleatoric information. 
+The system includes syntax for describing aleatoric information (in package langauge),
 Aletaoric knowledge bases are a generalisation of description logic knowledge bases,
 where concepts and roles are aleatoric, in that they are assumed to be determined by the role of a die.
 This allows fine grained modelling of probabilistic belief systems, with a rigorous mathematical foundation.
 
 
-The *tyche* package consists of the following modules:
-- **adl** this is the aleatoric description logic langauge module for representing formulas, performing proofs etc.
-- **models** this is the models module for creating schemas for individuals, assigning values to interpretations, serialising with a database, model checking, and learning.
-- **akb** this is the aleatoric knowledge base module for defining axiom schemas over the model schema, consistentcy checking and synthesis of interpretations.
-- **api** this is the abstract programming interface module for specifying models and axioms, perform learning and execute queries.
+The *tyche* package consists of the following subpackages and modules:
+- **language**. This is the aleatoric description logic langauge subpackage for representing formulas, and performing basic transformations.
+  It consists of the following modules:
+  - **adl**, a module for representing the syntax of the language, including:
+     - the logical operators *Concept*, *Conditional*, *Expectation* and *LeastFixedPoint*
+     - and the role operators *Role*, *Test*, *Iterate*, and *Combine*
+  - **proof**, a module consisting of theorem proving utilities.   
+- **logic**. This is the aleatoric description logic package for intensional reasoning. It consists of the modules:
+  - **axiom**, a module for representing beliefs of a universal nature, or TBooks.
+  - **assertion**, a module for representing beliefs pertsaiing to a specific individual, or ABooks.
+  - **theory**, a module for inference given axioms and assertions.
+- **models**. This is the module for representing extensional information, or a specific realisation of an aleatoric theory.
+  It provides a framework for writing python classes that represent aleatoric information constrained by aleatoric theories.
+  It also permits learning and database synchronisation operations.
+- **test** package will contain unit tests as they are written.
 
-The *test* package will contain unit tests as they are written.
-
-This page will be updated as these modeuls and packages are fleshed out.
+This page will be updated as these modules and packages are fleshed out.
 
 
--- tyche/adl.py
+++ tyche/adl.py
@@ -13,29 +13,131 @@ class ADLException(Exception):
     def __init__(self, message):
         self.message= "ADLException:" + message
 
+'''
+Refactor
+Simpler class system. No need for abstract classes of formula.
+The base classes are: No, Yes, Concept, Variable, Conditional, Expectation, LeastFixedPoint.
+Each formula class must provide the methods 
+__str__, 
+__repr__, 
+eval (acts on lambdas, returns a lambda)
+normal_form (produces a normal form of the formula)
+__equal__ (for an ordering of formulas)
+__lt__ (for ordering)
+
+'''
+class Concept:
+    '''
+    This class represents the atomic concepts Yes, No, and named concepts.
+    '''
+    def __init__(self, symbol):
+        if is_valid_symbol(symbol):
+            self.symbol = symbol
+
+    def is_valid_symbol(s):
+        '''Checks a string contains only alphanumeric characters or underscore'''
+        valid = s is not None and len(s)>0
+        if valid:
+            for c in s[1:]:
+                valid = valid and ((c.isalnum) or c=='_')
+        return valid    
+
+    '''
+    Class variables for Yes and No
+    '''
+    yes = Concept('Yes')
+    no = Concept('No')
+
+
+    def __str__(self):
+        return self.symbol
+
+    def __repr__(self):
+        return self.symbol
+
+    def __eq__(self, other):
+        return self.symbol == other.symbol
+        
+    def __lt__(self, other):
+        if self == no:
+            return True
+        if other == no:
+            return False
+        if self == yes:
+            return True
+        if other == yes:
+            return False
+        return self.symbol < other.symbol
+                        
+    def eval(self, concepts, roles):
+        '''returns the probability of a concept, 
+        given the lambda evaluation of roles and concepts.
+        It is assumed concepts is a function that 
+        maps concept symbols to probabilities, 
+        and roles is a function that maps role symbols
+        to probability ditributions of tuples of concept functions and role functions'''
+        if self == no:
+            return 0
+        if self == yes:
+            return 1
+        return concepts(symbol)
+        
+
+    #Proof theoretic functions here.
+    def normal_form(self):
+        '''Returns the tree normal form of a Concept, where atoms are ordered alphabetically.'''
+        return self
+
+    def is_equivalent(self, concept):
+        '''Returns true if this Concept is provably equivalent to adl_form'''
+        return self == concept
+
+    def is_weaker(self, concept):
+        '''Returns true if the probability of this Concept is provably 
+        necesssarily less than or equal to the probabiliy of adl_form'''
+        return concept == yes
+ 
+    def is_stronger(self, concept):
+        '''Returns true if the probability of this concept is provably
+        necessarily greater than or equal to the probability of adl_form'''
+        return concept == no
+
+
+
+####End of refactor######
+
 
 class Concept(tuple):
     '''
-    An class for representing all ADL concepts.
+    A class for representing all ADL concepts.
 
     Concepts are immutable and encoded via tuples, where the first term of each tuple indicates the type of formula.
     The constructor builds atomic concepts, and complex concepts are constructed using the static methods provided.
     '''
 
-    def __new__(cls, operator, symbol=None, term1=None, term2=None, term3=None, role=None):
-        '''Constructs all formulas as tuples'''
+    def __new__(cls, operator, *kwargs):
+        '''Constructs all formulas as tuples.
+        Usage is: 
+            c = Concept('No')
+            c = Concept('Yes')
+            c = Concept('Atom',symbol=s)
+            c = Concept('Conditional', condition=x, if_yes=y, if_no=n)
+            c = Concept('Expectation', role=r, concept=c)
+            c = Concept('LeastFixedPoint', variable=v, concept=c)
+        '''
         constructors = {#lambdas to construct formulas
-            'No': lambda c, s, x, y, z, r: tuple.__new__(c, ('No',)),
-            'Yes': lambda c, s, x, y, z, r: tuple.__new__(c, ('Yes',)),
-            'Atom': lambda c, s, x, y, z, r : tuple.__new__(c, ('Atom', s)),
-            'Conditional': lambda c, s, x, y, z, r: tuple.__new__(c, ('Conditional',x,y,z)),
-            'Marginal': lambda c, s, x, y, z, r: tuple.__new__(c, ('Marginal', r,x,y))
+            'No': lambda c, keys: tuple.__new__(c, ('No',)),
+            'Yes': lambda c, keys: tuple.__new__(c, ('Yes',)),
+            'Atom': lambda c, keys: tuple.__new__(c, ('Atom', keys['symbol'])),
+            'Conditional': lambda c, keys: tuple.__new__(c, ('Conditional', keys['condition'], keys['if_yes'], keys['if_no'])),
+            'Expectation': lambda c, keys: tuple.__new__(c, ('Expectation', keys['role'], keys['concept'] )
+            'LeastFixedPoint': lambda c, keys: tuple.__new__(c, ('LeastFixedPoint', keys['variable'], keys['concept'] )
             }#language core
         if operator=='Atom' and not cls.is_valid_symbol(symbol):
             raise ADLException('Symbols must begin with lowercase letter, and contain only alphanumeric characters or underscores')
-        if not operator in ['Yes','No','Atom','Conditional','Marginal']:
+        if not operator in ['Yes','No','Atom','Conditional','Expectation','LeastFixedPoint']:
             raise ADLException('Operator must be Yes, No, Atom, Conditional or Marginal')
-        return constructors.get(operator)(cls, symbol, term1, term2, term3, role)
+        return constructors.get(operator)(cls, kwargs)
 
 
     def is_valid_symbol(s):
@@ -51,46 +153,53 @@ class Concept(tuple):
     @property
     def operator(self):
         return self[0]
+
     @property
     def symbol(self):
         if self.operator=='Atom':
             return self[1]
         else:
             return None
+
     @property
     def condition(self):
         if self.operator=='Conditional':
             return self[1]
         else:
             return None
+
     @property
-    def positive(self):
+    def if_yes(self):
         if self.operator=='Conditional':
             return self[2]
         else:
             return None
+
     @property
-    def negative(self):
+    def if_no(self):
         if self.operator=='Conditional':
             return self[3]
         else:
             return None
+
     @property
     def role(self):
-        if self.operator=='Marginal':
+        if self.operator=='Expectation':
             return self[1]
         else:
             return None
+
     @property
-    def event(self):
-        if self.operator=='Marginal':
+    def concept(self):
+        if self.operator=='Expectation' or self.operator=="LeastFixedPoint":
             return self[2]
         else:
             return None
+
     @property
-    def margin(self):
-        if self.operator=='Marginal':
-            return self[3]
+    def variable(self):
+        if self.operator=='LeastFixedPoint':
+            return self[1]
         else:
             return None
     
@@ -105,11 +214,12 @@ class Concept(tuple):
         reps = {
             'No': lambda x: 'N',
             'Yes': lambda x: 'Y',
-            'Atom': lambda x: x[1],
-            'Conditional': lambda x: '({0}?{1}:{2})'.format(x[1].__repr__(), x[2].__repr__(), x[3].__repr__()),
-            'Marginal': lambda x: '.{0}({1}|{2})'.format(x[1].__repr__(),x[2].__repr__(),x[3].__repr__())
+            'Atom': lambda x: x.symbol,
+            'Conditional': lambda x: '({0}?{1}:{2})'.format(x.condition.__repr__(), x.if_yes.__repr__(), x.if_no.__repr__()),
+            'Expectation': lambda x: '[{0}]({1})'.format(x.role.__repr__(), x.concept.__repr__()),
+            'LeastFixedPoint': lambda x: 'LFP_{0}_({1})'.format(x.variable.__repr(), x.concept.__repr__()) 
             }
-        return reps.get(self[0])(self)
+        return reps.get(self.operator)(self)
 
     def pretty(self, indent=0):
         '''Returns a multiline formatted version of the formula.
@@ -123,34 +233,39 @@ class Concept(tuple):
                 'Atom': lambda x, y: x.symbol == y.symbol,
                 'Conditional': lambda x, y: \
                         x.condition==y.condition and \
-                        x.positive==y.positive and \
-                        x.negative==y.negative,
+                        x.if_yes==y.if_yes and \
+                        x.if_no==y.if_no,
                 'Marginal': lambda x, y:\
                         x.role==y.role and \
-                        x.event==y.event and \
-                        x.margin==y.margin
+                        x.concept==y.concept
+                'LeastFixedPoint': lambda x, y:\
+                        x.variable==y.variable and \
+                        x.concept==y.concept
                 }
         return self.operator==other.operator and \
                 eqs[self.operator](self,other)
                                 
                         
-
+    def eval(self):
+        '''returns a lambda to evaluate the Concept, 
+        given the lambda evaluation of roles and concepts'''
+        pass
 
     #Proof theoretic functions here.
-    def normal_form(self) -> 'Concept':
+    def normal_form(self):
         '''Returns the tree normal form of a Concept, where atoms are ordered alphabetically.'''
         pass
 
-    def is_equivalent(self, adl_form: 'Concept') -> bool:
+    def is_equivalent(self, adl_form: 'Concept'):
         '''Returns true if this Concept is provably equivalent to adl_form'''
         pass
 
-    def is_weaker(self, adl_form: 'Concept') -> bool:
+    def is_weaker(self, adl_form: 'Concept'):
         '''Returns true if the probability of this Concept is provably 
         necesssarily less than or equal to the probabiliy of adl_form'''
         pass
  
-    def is_stronger(self, adl_form: 'Concept') -> bool:
+    def is_stronger(self, adl_form: 'Concept'):
         '''Returns true if the probability of this concept is provably
         necessarily greater than or equal to the probability of adl_form'''
 
@@ -159,7 +274,7 @@ class Role(tuple):
     An class for representing all ADL roles.
 
     Roles are immutable and encoded via tuples. 
-    We currently just use atomic roles, but use these structures to allow for dynamic modalities in future.
+    We currently just use atomic roles, but will also represent dynamic roles.
     '''
     def __new__(cls, role):
         '''Creates an atomic concept, with the symbol symbol
@@ -190,28 +305,37 @@ class Role(tuple):
 #static constructors for different types of formulas
 yes = Concept('Yes')
 no = Concept('No')
+#define a concept to be a fair coin flip by convention?
+coin = Concept('Atom','fair_coin')
 #static functions for generating functions
 def atom(symbol):
     return Concept('Atom',symbol=symbol)
-def conditional(condition, positive, negative):
-    return Concept('Conditional', term1=condition, term2=positive, term3=negative)
-def marginal(role, event, margin):
-    return Concept('Marginal', term1=event, term2=margin, role=role)
+def conditional(condition, if_yes, if_no):
+    return Concept('Conditional', condition=condition, if_yes=if_yes, if_no=if_no)
+def expectation(role, concept):
+    return Concept('Exepctation', role=role, concept=concept)
+def least_fixed_point(variable, concept):
+    return Concept('LeastFixedPoint', variable=variable, concept=concept)
 #some abbreviations provided too
 def negation(term):
-    return Concept('Conditional', term1=term, term2=no, term3=yes)
+    return conditional(term, no, yes)
 def conjunction(left, right):
-    return Concept('Conditional', term1=left, term2=right, term3=no)
+    return conditional(left, right, no)
 def disjunction(left, right):
-    return Concept('Conditional', term1=left, term2=yes, term3=right)
+    return conditional(left, yes, right)
 def implication(antecedent, consequent):
-    return Concept('Conditional', term1=antecedent, term2=consequent, term3=yes)
+    return conditional(antecedent, consequent, yes)
 def equivalence(left, right):
-    return Concept('Conditional', term1=left, term2=right,term3=negation(right))
-def expectation(role, term):
-    return Concept('Marginal', term1=term, term2=yes, role=role)
+    return conditional(left, right, negation(right))
+
+#ongoing
+def marginal(role, concept, margin):#hmmm need a way to avoid name clashes and check for monotonicity.
+    variable = atom('x')
+    return least_fixed_point(atom('x'),conditional(coin,expectation(conjunction(concept,margin))
+
+
 def necessity(role, term):
     return Concept('Marginal', term1=no, term2=negation(term), role=role)
-def possibility(role, term):
+def greatest_fixed_point(variable,  concept):
     return negation(Concept('Marginal', term1=no, term2=term, role=role))
 
